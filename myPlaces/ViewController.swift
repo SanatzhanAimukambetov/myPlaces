2404cb89f7773bde1a755f1cac0f684b14812b12
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-10-21 20:33:12 +0600

    Initial Commit

---
+++ myPlaces/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  myPlaces
+//
+//  Created by Kairat Zukhra on 21.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
