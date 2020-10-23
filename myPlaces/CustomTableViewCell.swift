ac4a14356c46d829c04bc332718c8edc8951b8d4
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-10-23 16:50:14 +0600

    Custom Table View Cell

---
+++ myPlaces/CustomTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  CustomTableViewCell.swift
+//  myPlaces
+//
+//  Created by Kairat Zukhra on 23.10.2020.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    @IBOutlet var imageOfPlace: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var locationLabel: UILabel!
+    @IBOutlet var typeLabel: UILabel!
+    
+}
