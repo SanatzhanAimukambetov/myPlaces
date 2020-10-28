dcf22962b9fe715423a590969335fcfe7e707c2b
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-10-28 14:21:08 +0600

    Realm forever rearrange PlaceModel for Realm create new file StorageManager

--- myPlaces/PlaceModel.swift
+++ myPlaces/PlaceModel.swift
@@ -5,31 +5,35 @@
 //  Created by Kairat Zukhra on 23.10.2020.
 //
 
-import UIKit
+import RealmSwift
 
-struct Place {
+class Place: Object {
     
-    var name: String
-    var location: String?
-    var type: String?
-    var image: UIImage?
-    var restaurantImage: String?
+    @objc dynamic var name = ""
+    @objc dynamic var location: String?
+    @objc dynamic var type: String?
+    @objc dynamic var imageData: Data?
     
-    static let restaurantsNames = [
+    let restaurantsNames = [
         "Doner", "Пицца Блюз", "KFC", "Meiman",
         "Sushimania", "Dodo Pizza", "barBQ", "Tulipark",
         "Lanzhou", "Waffle house", "Appetite", "Дастархан",
         "Crystal Detailing", "JOY VOSTOK", "Золотая Орда"
     ]
     
-    static func getPlaces() -> [Place] {
-        
-        var places = [Place]()
+    func savePlaces() {
         
         for place in restaurantsNames {
-            places.append(Place(name: place, location: "Усть-Каменогорск", type: "Ресторан", image: nil, restaurantImage: place))
+            
+            let image = UIImage(named: place)
+            guard let imageData = image?.pngData() else { return }
+            
+            let newPlace = Place()
+            
+            newPlace.name = place
+            newPlace.location = "Oskemen"
+            newPlace.type = "Restaurant"
+            newPlace.imageData = imageData
         }
-        
-        return places
     }
 }
---
+++ myPlaces/StorageManager.swift
@@ -0,0 +1,20 @@
+//
+//  StorageManager.swift
+//  myPlaces
+//
+//  Created by Kairat Zukhra on 28.10.2020.
+//
+
+import RealmSwift
+
+let realm = try! Realm()
+
+class StorageManager {
+    
+    static func saveObject(_ place: Place) {
+        
+        try! realm.write {
+            realm.add(place)
+        }
+    }
+}
