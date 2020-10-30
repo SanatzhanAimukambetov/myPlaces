b0bf1b63854fb501fdfd7a541b8bcee8dafefc09
Sanatzhan Aimukambetov <sanatzhan.aimukambetov@nu.edu.kz>
2020-10-30 20:30:30 +0600

    Button was added to stack view through code

---
+++ myPlaces/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  myPlaces
+//
+//  Created by Kairat Zukhra on 30.10.2020.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initialization
+    
+    override  init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupButtons() {
+        
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = .red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+}
